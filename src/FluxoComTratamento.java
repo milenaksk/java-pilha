c88a48a451729d9532b3baaf8ff6f70c4c0cdfc6
milenaksk <cristinemilena32@gmail.com>
2022-12-19 12:24:08 -0300

    hierarquia excecoes

--- src/FluxoComTratamento.java
+++ src/FluxoComTratamento.java
@@ -1,9 +1,9 @@
-public class Fluxo {
+public class FluxoComTratamento {
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
             metodo1();
-        } catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
+        } catch (ArithmeticException | NullPointerException ex) {
             String msg = ex.getMessage();
             System.out.println("Exception " + msg);
             ex.printStackTrace();
@@ -19,11 +19,12 @@ public class Fluxo {
 
     public static void metodo2() {
         System.out.println("Ini do metodo2");
-        Conta c = new Conta();
-
-        throw new MinhaExcecao("DEU MUITO ERRADO");
-
-
-        //System.out.println("Fim do metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            //int a = i / 0;
+            Conta c = null;
+            c.deposita();
+        }
+        System.out.println("Fim do metodo2");
     }
 }
